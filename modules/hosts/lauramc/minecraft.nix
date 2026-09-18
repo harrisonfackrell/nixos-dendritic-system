28c5e789b55ebea4dad9e7a0c8578db04fd60808
harrisonfackrell <20hfackrell@gmail.com>
2026-09-17 21:49:11 -0600

    Revert "lauramc - Update Aether II URL"
    This reverts commit 176327a9a06ab209066ea81111599d2a4fda07d1.

--- modules/hosts/lauramc/minecraft.nix
+++ modules/hosts/lauramc/minecraft.nix
@@ -23,8 +23,8 @@
                     mods = pkgs.linkFarmFromDrvs "mods" (
                         builtins.attrValues {
                             Aether-II = pkgs.fetchurl {
-                                url = "https://cdn.modrinth.com/data/JD2NSu5O/versions/soZ6Sulf/aether_ii-26.1.2-alpha.4.1-neoforge.jar";
-                                sha512 = "9bP86hcbglTl76HP35AAXbeOVCHzI79DdK8FqtMgstT5cJlU8Xc5HLVnsattb3w1Xu5yuB+STjTqEf9B5MBjgQ==";
+                                url = "https://cdn.modrinth.com/data/JD2NSu5O/versions/Qsxsn6rl/aether_ii-26.1.2-alpha.4-neoforge.jar";
+                                sha512 = "vsOTEtBr4STB00xwl9fGokf3G+Z5wupNNshtYoEI7uYvh74ApBltRJvpbEYQDU7fpTfSCnycjCPCP8+DJiJ/ug==";
                             };
                         }
                     );
